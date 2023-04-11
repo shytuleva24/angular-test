00cf3948c9ce07f05bcda218adfbf32c4cc7794e
shytuleva24 <shutyleva@gmail.com>
2023-04-11 15:20:32 +0300

    angular-HttpClient lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
+import {delay} from "rxjs/operators";
+import {Todo, TodosService} from "./todos.service";
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+
+    todos: Todo[] = [];
+
+    todoTitle:string = '';
+    loading: boolean = false;
+    error: string = '';
+
+    constructor(private todoService: TodosService) {
+    }
+
+    ngOnInit(): void {
+        this.fetchTodos();
+    }
+
+    addTodo(): void {
+        if (!this.todoTitle.trim()) {
+            return;
+        }
+        this.todoService.addTodo({
+            title: this.todoTitle,
+            completed: false
+        }).subscribe(todo => {
+            this.todos.push(todo);
+            this.todoTitle = '';
+        })
+    }
+
+    fetchTodos(): void {
+        this.loading = true;
+        this.todoService.fetchTodos()
+            .subscribe(todos => {
+                // console.log('Response', todos);
+                this.todos = todos;
+                this.loading = false;
+            }, error => {
+                console.log(error.message);
+                this.error = error.message;
+            })
+    }
+
+    removeTodo(id: number): void {
+        this.todoService.removeTodo(id)
+            .subscribe(() => {
+                this.todos = this.todos.filter(t => t.id !== id);
+            })
+    }
+
+    completeTodo(id: number) {
+        this.todoService.completeTodo(id)
+            .subscribe(todo => {
+                // todo = JSON.parse(todo);
+                this.todos.find(t => t.id === todo.id).completed = true;
+            })
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Provider} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AuthInterceptor} from "./auth.interceptor";
 
+const INTERCEPTOR_PROVIDER: Provider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+}
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+    ],
+    providers: [INTERCEPTOR_PROVIDER],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+
+}
