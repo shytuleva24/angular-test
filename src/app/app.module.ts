58152d14e7e41e28e37b5259ff40bfb45fd788c1
shytuleva24 <shutyleva@gmail.com>
2023-04-12 12:02:59 +0300

    angular-HttpClient lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+
+  ],
+  bootstrap: [
+      AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
