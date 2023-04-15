5f59967e581a8fbae90fd1c6854327af6a778a59
shytuleva24 <shutyleva@gmail.com>
2023-04-15 15:54:55 +0300

    angular-dinamic component lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,36 @@
-import {Component} from '@angular/core'
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
+import {ModalComponent} from "./modal/modal.component";
+import {RefDirective} from "./ref.directive";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  modal = false
+    @ViewChild(RefDirective, {static: false}) refDir: RefDirective
+    constructor(
+        private resolver: ComponentFactoryResolver,
+        private title: Title,
+        private meta: Meta
+    ) {
+        title.setTitle('App Component Page');
+        meta.addTags([
+            {name: 'keywords', content: 'angular,google,appcomponent'},
+            {name: 'description', content: 'this is app component'}
+        ])
+    }
+
+    showModal() {
+        const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
+        this.refDir.containerRef.clear()
+
+        const component = this.refDir.containerRef.createComponent(modalFactory)
+        component.instance.title = 'Dinamic title'
+        component.instance.close.subscribe(() => {
+            this.refDir.containerRef.clear()
+        })
+    }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms';
+import { ModalComponent } from './modal/modal.component'
+import {RefDirective} from "./ref.directive";
 
 @NgModule({
   declarations: [
     AppComponent,
+    ModalComponent,
+    RefDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
   providers: [],
+  entryComponents: [ModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
