f3ce7e6886503caaf46a623cd71005af2c2bedf0
shytuleva24 <shutyleva@gmail.com>
2023-04-15 14:39:24 +0300

    angular-modules lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+
+import {AppComponent} from './app.component'
+import {FormsModule} from '@angular/forms'
+import {HomePageComponent} from './home-page/home-page.component'
+import {AppRoutingModule} from './app-routing.module'
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
-  declarations: [
-      AppComponent
-  ],
-  imports: [
-      BrowserModule
-  ],
-  providers: [
+    declarations: [
+        AppComponent,
+        HomePageComponent,
 
-  ],
-  bootstrap: [
-      AppComponent
-  ]
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        SharedModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
