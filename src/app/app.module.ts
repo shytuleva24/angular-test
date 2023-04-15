7da216f7fdfa145fb7e82d271a79ce1dea85d000
shytuleva24 <shutyleva@gmail.com>
2023-04-15 15:01:04 +0300

    angular-modules lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+
+import {AppComponent} from './app.component'
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-      AppComponent
+    AppComponent,
   ],
   imports: [
-      BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
-  providers: [
-
-  ],
-  bootstrap: [
-      AppComponent
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
