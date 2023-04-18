02c70e9c1bae00d273b036a565d27fc70e82b4d9
shytuleva24 <shutyleva@gmail.com>
2023-04-18 13:41:14 +0300

    angular-animations lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import {Component} from '@angular/core'
+import {Component} from '@angular/core';
+import {boxAnimation} from "./app.animations";
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    animations: [ boxAnimation ]
 })
 export class AppComponent {
-  modal = false
-}
+    boxState = 'start'
+    visible = true
 
+    animate() {
+        this.boxState = this.boxState === 'end' ? 'start' : 'end'
+    }
+
+    animationStarted(event: AnimationEvent) {
+        console.log('animation started ', event)
+    }
+
+    animationDone(event: AnimationEvent) {
+        console.log('animation done ', event)
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { AnimateComponent } from './animate/animate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AnimateComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
